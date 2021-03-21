76dff862d700b8d86be3485e77964642c61f01b9
1WojciechNowak <4WojciechNowak@gmail.com>
2021-03-21 12:23:13 +0100

    Implementation of Transforming Logic & TransformingSpec

--- src/main/scala/ai/wojciechnowak/app/functions/Transforming.scala
+++ src/main/scala/ai/wojciechnowak/app/functions/Transforming.scala
@@ -1,5 +1,36 @@
 package ai.wojciechnowak.app.functions
 
+import ai.wojciechnowak.app.model.{IPAddress, IPNumber, Input, Scope}
+
+import scala.util.{Failure, Try}
+import scala.util.matching.Regex
+
 trait Transforming {
 
+  // Simplified version of IPV4 regex. It is not enough to validate IPV4 with Regex below!
+  private val IPV4_PATTERN: Regex = raw"""([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})""".r
+
+  def inputToIpAddressScope(input: Input): Try[Scope[IPAddress]] =
+    inputTransformer(input)(toIPAddressTransformer)
+
+  def ipAddressScopeToIpNumberScope(ipAddresses: Scope[IPAddress]): Scope[IPNumber] =
+    tupleTransformer(ipAddresses)(ipv4ToNumberTransformer)
+
+  private def inputTransformer[T](input: Input)(f: String => Try[T]): Try[Scope[T]] = for {
+    left <- f(input.left)
+    right <- f(input.right)
+  } yield Scope(left, right)
+
+  private val toIPAddressTransformer: String => Try[IPAddress] = {
+    case IPV4_PATTERN(a, b, c, d) => IPAddress(a, b, c, d)
+    case _ => Failure(new IllegalArgumentException("Paring error. " +
+      "At lease one of arguments do not fit to IPV4 pattern."))
+  }
+
+  private def tupleTransformer[A, B](scope: Scope[A])(f: A => B): Scope[B] = scope match {
+    case Scope(s, e) => Scope(f(s), f(e))
+  }
+
+  private val ipv4ToNumberTransformer: IPAddress => IPNumber = ipv4 => IPNumber(ipv4)
 }
+
--- src/test/scala/ai/wojciechnowak/app/functions/TransformingSpec.scala
+++ src/test/scala/ai/wojciechnowak/app/functions/TransformingSpec.scala
@@ -1,5 +1,52 @@
 package ai.wojciechnowak.app.functions
 
-class TransformingSpec {
+import ai.wojciechnowak.app.model.{IPAddress, Input, Scope}
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpecLike
 
+class TransformingSpec extends AnyWordSpecLike
+  with Matchers
+  with Transforming {
+
+  "Calling inputToIPAddress" should {
+    "succeed with transform input to Scope of IPAddress" in {
+      val testInput = Input("201.233.7.160", "201.233.7.168")
+
+      val ipAddressTuple = inputToIpAddressScope(testInput)
+      assert(ipAddressTuple.isSuccess)
+      ipAddressTuple.get.start shouldBe IPAddress(201,233,7,160)
+      ipAddressTuple.get.end shouldBe IPAddress(201,233,7,168)
+    }
+
+    "fail with transform input to Scope of IPAddress on wrong both 'input.left' and 'input.right'" in {
+      val testInput = Input("19X.233.OP.1D8", "blah")
+
+      val ipAddressTuple = inputToIpAddressScope(testInput)
+      assert(ipAddressTuple.isFailure)
+    }
+
+    "fail with transform input to Scope of IPAddress on wrong 'input.left'" in {
+      val testInput = Input("blah", "201.233.7.168")
+
+      val ipAddressTuple = inputToIpAddressScope(testInput)
+      assert(ipAddressTuple.isFailure)
+    }
+
+    "fail with transform input to Scope of IPAddress on wrong 'input.right'" in {
+      val testInput = Input("197.233.7.168", "blah")
+
+      val ipAddressTuple = inputToIpAddressScope(testInput)
+      assert(ipAddressTuple.isFailure)
+    }
+  }
+
+  "Calling ipAddressTupleToIpNumberTuple" should {
+    "transform ipAddress to ipNumber" in {
+      val testIPAddressTuple = Scope(IPAddress(197,233,7,168), IPAddress(201,233,7,160))
+
+      val ipNumberTuple = ipAddressScopeToIpNumberScope(testIPAddressTuple)
+      ipNumberTuple.start.number shouldBe 3320383400L
+      ipNumberTuple.end.number shouldBe 3387492256L
+    }
+  }
 }
