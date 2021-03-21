c5f3c39e5ffa87eed08d69a2c61f66c0841a955d
1WojciechNowak <4WojciechNowak@gmail.com>
2021-03-21 12:28:50 +0100

    Usage of transformations in main pipeline

--- src/main/scala/ai/wojciechnowak/app/MainBoot.scala
+++ src/main/scala/ai/wojciechnowak/app/MainBoot.scala
@@ -1,6 +1,24 @@
 package ai.wojciechnowak.app
 
-object MainBoot extends App {
+import ai.wojciechnowak.app.functions.Transforming
+import ai.wojciechnowak.app.model.{IPAddress, IPNumber, Input, Scope}
+import org.apache.spark.sql.Dataset
 
-  println("Hello, world!")
+import ai.wojciechnowak.app.Config._
+
+object MainBoot extends App with Transforming{
+
+  import Env.spark.implicits._
+  val inputDS: Dataset[Input] = InputReader
+    .readCsv("simple_input")
+    .as[Input]
+
+  val ipAddressTupleDS: Dataset[Scope[IPAddress]] =
+    inputDS.map(inputToIpAddressScope)
+      .flatMap(_.toOption)
+
+  val ipNumberTupleDS: Dataset[Scope[IPNumber]] =
+    ipAddressTupleDS.map(ipAddressScopeToIpNumberScope)
+  
+  ipNumberTupleDS.show()
 }
