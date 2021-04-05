7d414195098a471ab8058a2f0b392b8cb9763432
1WojciechNowak <4WojciechNowak@gmail.com>
2021-04-05 23:18:12 +0200

    Add logic to MainBoot with pipeline

--- src/main/scala/ai/wojciechnowak/app/MainBoot.scala
+++ src/main/scala/ai/wojciechnowak/app/MainBoot.scala
@@ -1,18 +1,19 @@
 package ai.wojciechnowak.app
 
-import ai.wojciechnowak.app.functions.Transforming
+import ai.wojciechnowak.app.functions.{Merging, Reducing, Transforming}
 import ai.wojciechnowak.app.model.{IPAddress, IPNumber, Input, Scope}
 import org.apache.spark.sql.Dataset
-
 import ai.wojciechnowak.app.Config._
 
-object MainBoot extends App with Transforming {
-
+object MainBoot extends App with Transforming with Merging with Reducing {
   import Env.spark.implicits._
+
+  // Data Loading
   val inputDS: Dataset[Input] = InputReader
-    .readCsv("simple_input")
+    .readCsv("sample_input")
     .as[Input]
 
+  // Preprocessing
   val ipAddressScopeDS: Dataset[Scope[IPAddress]] =
     inputDS.map(inputToIpAddressScope)
       .flatMap(_.toOption)
@@ -20,5 +21,16 @@ object MainBoot extends App with Transforming {
   val ipNumberScopeDS: Dataset[Scope[IPNumber]] =
     ipAddressScopeDS.map(ipAddressScopeToIpNumberScope)
 
-  ipNumberScopeDS.show()
+  val ipNumberSets: Dataset[IPNumber] = ipNumberScopeDS
+    .flatMap(ipNumberScopeToIpNumberSet)
+
+  // Reducing & Merging
+  val exclusiveNumbers: Dataset[IPNumber] = reduce.apply(ipNumberSets)
+  val result: Dataset[Scope[IPNumber]] = merge.apply(exclusiveNumbers)
+
+  // Postprocessing
+  val resultAsIPAddress: Dataset[Scope[IPAddress]] = result.map(ipNumberScopeToIpAddressScope)
+
+  // Presenting results
+  resultAsIPAddress.show(false)
 }
